4e4c5edd9091d20cea773dbdb64f9566846788bd
mlogallo <85250289+mlogallo@users.noreply.github.com>
2022-03-23 17:18:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "iksws" {
   backend = "remote"
   config = {
-    organization = "Netnology"
+    organization = "Netnology-MINT"
     workspaces = {
       name = var.ikswsname 
     }
